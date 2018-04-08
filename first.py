ba10c728f3826bdf06711d635a35a25bf8011679
urshala <lahiripak@gmail.com>
2018-04-08 23:57:07 +0300

    Added developer and manager subclasses

--- first.py
+++ first.py
@@ -12,8 +12,37 @@ class Employee(object):
 		return self.first_name
 		
 		
-emp_1 = Employee('Deepak', 'Panta', 15000)
-emp_2 = Employee('Test', 'User', 16000)
 
-print (emp_1.email)
-print (emp_1.email)
+
+
+#inheritance added by ursh
+
+
+#Manager and developer class
+class Developer(Employee):
+	annual_raise = 9
+
+	def __init__(self,first_name,last_name,pay, lang):
+		super().__init__(first_name, last_name, pay)
+		self.lang = lang
+
+
+class Manager(Employee):
+	def __init__(self, first_name,last_name,pay, employees=None):
+		super().__init__(first_name, last_name, pay)
+		if employees is None:
+			self.employees = []
+		else:
+			self.employees = employees
+
+	def add_employee(self, emp):
+		if emp not in self.employees:
+			self.employees.append(emp)
+
+	def remove_employee(self, emp):
+		if emp in self.employees:
+			self.employees.remove(emp)
+
+	def print_emp(self):
+		for em in self.employees:
+			print('----->', em)
