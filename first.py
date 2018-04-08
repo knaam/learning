dc9c062b3b16feaec963437ea5a754ecb6f29336
knaam <elloxel@gmail.com>
2018-04-08 23:08:24 +0300

    Added static and class methods

--- first.py
+++ first.py
@@ -1,5 +1,10 @@
 class Employee(object):
 	"""docstring for Employee"""
+	#user 1 will add class variables
+
+	company_name = 'XYZ Software Oy'
+	annual_raise = 0.3
+
 	def __init__(self, first_name, last_name, pay):
 		self.first_name = first_name
 		self.last_name = last_name
@@ -7,13 +12,29 @@ class Employee(object):
 		self.pay = pay
 
 
+	def apply_raise(self):
+		return Employee.annual_raise * self.pay
 
 	def __str__(self):
 		return self.first_name
-		
-		
-emp_1 = Employee('Deepak', 'Panta', 15000)
-emp_2 = Employee('Test', 'User', 16000)
 
-print (emp_1.email)
-print (emp_1.email)
+	@classmethod
+	def set_raise(cls, new_raise):
+		cls.annual_raise = new_raise
+
+	@classmethod
+	def from_string(cls,employee_string):
+		fname, lname, pay = employee_string.split('-')
+		return cls(fname, lname, pay)
+
+	@classmethod
+	def from_comma_seperated(cls,comma_seperated):
+		fname,lname,pay=comma_seperated.split(',')
+		return cls(fname,lname,pay)
+
+	@staticmethod
+	def is_working_day(num):
+		if num == 23:
+			return True
+		return False
+		
